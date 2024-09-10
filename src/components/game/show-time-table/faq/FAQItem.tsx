2f433ea098710ea5663ef0ba9984c76eff854622
JongMany <blackberry1114@naver.com>
2024-09-10 09:05:27 +0900

    feat: FAQ 토글 기능 구현

--- src/components/game/show-time-table/faq/FAQItem.tsx
+++ src/components/game/show-time-table/faq/FAQItem.tsx
@@ -6,11 +6,12 @@ type Props = {
   question: string;
   answers: string[];
   isOpened: boolean;
+  onClick: () => void;
 }
-export const FAQItem = ({question, answers, isOpened}: Props) => {
+export const FAQItem = ({question, answers, isOpened, onClick}: Props) => {
   return (
       <div className={"flex flex-col gap-[12px] cursor-pointer break-keep"}>
-        <div className={"flex font-[600] text-[16px] leading-[26px] text-[rgb(20,22,23)]"}>
+        <div className={"flex font-[600] text-[16px] leading-[26px] text-[rgb(20,22,23)]"} onClick={onClick}>
           <p className={"text-[rgb(255,108,122)]"}>Q.</p>
           <p className={"w-full mr-[12px] ml-[8px] text-[rgb(58,62,65)]"}>{question}</p>
           {isOpened ? <UpArrow/> : <DownArrow/>}
