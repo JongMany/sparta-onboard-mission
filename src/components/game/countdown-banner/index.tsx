988127427123be2df8be09beaa03c8a6d09f0cd6
JongMany <blackberry1114@naver.com>
2024-09-11 10:22:23 +0900

    feat: countdown timer 모바일 대응

--- src/components/game/countdown-banner/index.tsx
+++ src/components/game/countdown-banner/index.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
+import {RecruitmentDeadlineTimer} from "@/components/game/countdown-banner/timer/RecruitmentDeadlineTimer";
 
 // 9월 12일 목 24:00까지
 export const CountdownBanner = () => {
   return (
       <section className={"relative"}>
-        <section className={"bg-[rgb(20,22,23)] py-[100px]"}>
-          <main className={"flex mx-auto justify-between max-w-[796px] items-center"}>
+        <section className={"bg-[rgb(20,22,23)] lg:py-[100px] lg:px-[0px] py-[80px] px-[16px] block"}>
+          <main className={"flex flex-col lg:flex-row  mx-auto justify-center lg:justify-between max-w-[328px] lg:max-w-[796px] items-start lg:items-center"}>
             <div>
-              <h1 className={"font-[700] text-[32px] leading-[45px] text-left text-white"}>⏰ 모집 마감 임박 ⏰</h1>
-              <div className={"text-[18px] leading-[29px] mt-[8px] text-[rgb(157,167,174)] opacity-80 whitespace-pre-wrap"}>곧 올해 마지막 모집이 마감됩니다.</div>
+              <h1 className={"relative font-[700] text-[24px] lg:text-[32px] leading-[34px] lg:leading-[45px] text-left text-white"}>⏰ 모집 마감 임박 ⏰</h1>
+              <div className={"hidden lg:text-[18px] lg:leading-[29px] lg:mt-[8px] lg:text-[rgb(157,167,174)] lg:opacity-80 lg:whitespace-pre-wrap"}>곧 올해 마지막 모집이 마감됩니다.</div>
             </div>
-            <div className={"leading-[150%] font-[700] flex justify-center items-center text-[40px] text-white"}>
-              <div className={"w-[117px] h-[76px] mr-[12px] ml-[4px] py-[8px] rounded-[6px] text-[rgb(20,22,23)] flex text-center justify-center items-center bg-white"}>3일</div>
-              <div className={"w-[76px] h-[76px] text-center rounded-[8px] flex justify-center items-center font-[700] mx-[4px] text-[rgb(20,22,23)] bg-[rgba(255,255,255,0.8)]"}>07</div>
-              :
-              <div className={"w-[76px] h-[76px] text-center rounded-[8px] flex justify-center items-center font-[700] mx-[4px] text-[rgb(20,22,23)] bg-[rgba(255,255,255,0.8)]"}>50</div>
-              :
-              <div className={"w-[76px] h-[76px] text-center rounded-[8px] flex justify-center items-center font-[700] mx-[4px] text-[rgb(20,22,23)] bg-[rgba(255,255,255,0.8)]"}>24</div>
-            </div>
-            <div className={"hidden"}>곧 올해 마지막 모집이 마감됩니다.</div>
+            <RecruitmentDeadlineTimer />
+            <div className={"lg:hidden block text-[14px] leading-[22px] mt-[8px] text-[rgb(157,167,174)] opacity-80 whitespace-pre-wrap"}>곧 올해 마지막 모집이 마감됩니다.</div>
           </main>
         </section>
       </section>
---
+++ src/components/game/countdown-banner/timer/RecruitmentDeadlineTimer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const RecruitmentDeadlineTimer = () => {
+  return (
+      <div className={"leading-[150%] font-[700] flex justify-center items-center text-[28px] lg:text-[40px] text-white lg:m-0 my-[20px]"}>
+        <div className={"w-[88px] lg:w-[117px] h-[54px] lg:h-[76px] lg:mr-[12px] mr-[12px] lg:ml-[4px] ml-0 py-[6px] lg:py-[8px] rounded-[6px] text-[rgb(20,22,23)] flex text-center justify-center items-center bg-white"}>3일</div>
+        <div className={"w-[54px] h-[54px] lg:w-[76px] lg:h-[76px] py-[6px] lg:py-[8px] text-center rounded-[8px] flex justify-center items-center font-[700] mx-[4px] text-[rgb(20,22,23)] bg-[rgba(255,255,255,0.8)]"}>07</div>
+        :
+        <div className={"w-[54px] h-[54px] lg:w-[76px] lg:h-[76px] py-[6px] lg:py-[8px] text-center rounded-[8px] flex justify-center items-center font-[700] mx-[4px] text-[rgb(20,22,23)] bg-[rgba(255,255,255,0.8)]"}>50</div>
+        :
+        <div className={"w-[54px] h-[54px] lg:w-[76px] lg:h-[76px] py-[6px] lg:py-[8px] text-center rounded-[8px] flex justify-center items-center font-[700] mx-[4px] text-[rgb(20,22,23)] bg-[rgba(255,255,255,0.8)]"}>24</div>
+      </div>
+  );
+};
+
