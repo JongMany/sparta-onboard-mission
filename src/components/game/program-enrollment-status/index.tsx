0c0527f62e76648503cbc809d02fdd47e4111a97
JongMany <blackberry1114@naver.com>
2024-09-11 10:26:13 +0900

    feat: enrollment 모바일 대응

--- src/components/game/program-enrollment-status/index.tsx
+++ src/components/game/program-enrollment-status/index.tsx
@@ -8,7 +8,10 @@ export const ProgramEnrollmentStatusSection = () => {
       <section id={"gameWPN"} className={"relative"}>
         <section className={"flex justify-center bg-[rgb(20,22,23)]"}>
           <picture className={"hidden"}></picture>
-          <picture className={"max-w-[820px] w-full"}>
+          <picture className={"w-full lg:hidden"}>
+            <Image src={MobileProgramEnrollmentStatusImage} alt={"등록 정보"} className={"unset object-cover w-full h-full inset-0 text-transparent object-[center_top]"} />
+          </picture>
+          <picture className={"hidden lg:max-w-[820px] lg:w-full"}>
             <Image src={PcProgramEnrollmentStatusImage} alt={"등록 정보"} className={"object-cover w-full h-full inset-0 text-transparent object-[center_top]"} />
           </picture>
         </section>
