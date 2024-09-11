1fba117d3ef1265b5303f9f4d452dd433e717f00
JongMany <blackberry1114@naver.com>
2024-09-11 10:48:15 +0900

    feat: whyBecomeDeveloper 영역 모바일 대응 구현

--- src/components/game/why-become-developer/index.tsx
+++ src/components/game/why-become-developer/index.tsx
@@ -8,10 +8,10 @@ export const WhyBecomeDeveloperSection = () => {
   return (
       <section id={"gameWhyNow"} className={"relative"}>
         <section className={"flex justify-center bg-[rgb(20,22,23)]"}>
-          <picture className={"hidden"}>
-            <Image src={MobileWhyBecomeDeveloperImage} alt={"왜 게임개발자여야 하나"}/>
+          <picture className={"lg:hidden w-full"}>
+            <Image src={MobileWhyBecomeDeveloperImage} alt={"왜 게임개발자여야 하나"} className={"object-cover w-full h-full inset-0 text-transparent object-[center_top]"}/>
           </picture>
-          <picture className={"w-full max-w-[820px]"}>
+          <picture className={"hidden lg:w-full lg:max-w-[820px]"}>
             <Image src={PcWhyBecomeDeveloperImage} alt={"왜 게임개발자여야 하나"} className={"object-cover w-full h-full inset-0 text-transparent object-[center_top]"}/>
           </picture>
         </section>
